f3647c2ce0ddc1017cbfc12acafae6161f9dd698
Renato Ruiz <renatoruiz2607@gmail.com>
2021-03-23 19:52:44 -0300

    Iniciado projeto utilizando create-react-app - Criado Header e as 4 primeiras sessões da página.

---
+++ src/App.jsx
@@ -0,0 +1,327 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="/images/Grupo 368@2x.png" alt="headerLogoImg" />
+        <a href="">Projects</a>
+        <a href="">Partners</a>
+        <a href="">FAQ</a>
+        <a href="">Masters</a>
+        <a href="">Classes</a>
+        <a href="">Course Experience</a>
+        <a href="">About us</a>
+        <a href="">Register</a>
+      </header>
+
+      <div class="firstBackgroundBlock"></div>
+
+      <div class="productDesignBox">
+        <div class="divArrowLeft">
+          <i class="arrowLeft"></i>
+        </div>
+        <div>
+          <span class="productDesignTitle">Product Design Course</span>
+          <div>
+            <span class="productFactoryTitle">Product Factory</span>
+          </div>
+          <span class="productDesignText">Learn how design thinking, user research, business vision and marketing, and finally designing and creating real digital products for real users.</span>
+          <a href="" class="productDesingButton">Start Register</a>
+        </div>
+        <div>
+          <img src="/images/Screen Shot 2021-03-22 at 23.34.52.png" alt="productDesignImg" class="productDesignImg" />
+        </div>
+        <div class="divArrowRight">
+          <i class="arrowRight"></i>
+        </div>
+      </div>
+
+      <div class="secondBackgroundBlock"></div>
+
+      <div class="aboutUsBox">
+        <div class="aboutUsUpper">
+          <div class="productFactoryBox">
+            <div class="productFactoryTv">
+              <img src="/images/Grupo 368@2x.png" alt="productFactoryTvImg" class="productFactoryTvImg" />
+              <img src="/images/Grupo 602@2x.png" alt="productFactoryPlayImg" class="productFactoryPlayImg" />
+            </div>
+          </div>
+          <div class="aboutUsTitleBox">
+            <span class="aboutUsTitle">About us</span>
+            <span class="aboutUsTitleBackground">About</span>
+          </div>
+          <div class="aboutUsLinks">
+            <div class="aboutUsLinksAlign">
+              <a href=""><img src="/images/facebook-square-brands@2x.png" alt="facebookImg" /></a>
+              <a href=""><img src="/images/twitter-brands@2x.png" alt="twitterImg" /></a>
+              <a href=""><img src="/images/instagram-brands(1)@2x.png" alt="instagramImg" /></a>
+            </div>
+          </div>
+        </div>
+        <div class="aboutUsBackground">
+          <span class="aboutUsText">Today, given the economic and labor market conditions, the best and least risky way to invest is to invest in personal assets and skills. UI / UX Product Design Course covers all aspects of product design from product thinking and design to user-centric research and product research and business vision and digital marketing to product designers and creators, designing the end-user experience and interface of the product and how to deliver it. Includes. In this course, we have tried to use the presence of successful and experienced experts in each of the above mentioned areas to engage in more specialized interaction as a mentor. This course is project-oriented and participants in several product teams are working to improve their product quality. And then start competing with other teams in a healthy, learning-filled competition. Finally, after the final exam and competition, participants will be awarded a Certificate of Product Factory certification.</span>
+        </div>
+      </div>
+
+      <div class="courseExperienceBox">
+        <div class="courseExperienceTitleBox">
+          <span class="courseExperienceTitle">Course Experience</span>
+          <span class="courseExperienceTitleBackground">Experience</span>
+        </div>
+        <div class="courseExperienceButtonsBox">
+          <button class="afterButton">After attending the course</button>
+          <button class="duringButton">During the period</button>
+          <button class="beforeButton">Before attending the course</button>
+        </div>
+        <div class="courseExperienceBottomAlign">
+          <div class="courseExperienceBottomBox">
+            <img src="/images/Grupo 468@2x.png" alt="courseExperienceImg" class="courseExperienceImg" />
+            <span>This course has been attempted by zero people who are eager to learn product design, especially user experience and user interface, so it is not a prerequisite, but due to the limited capacity of the priority course with early enrolled people, others can participate in future courses.<br /> Obviously, the ability to use tools like Adobe Photoshop and Adobe Illustrator as well as Microsoft Word, Excel tools, and of course a lot of enthusiasm and energy,</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="thirdBackgroundBlock"></div>
+
+      <div class="sessionsBox">
+
+        <div class="sessionsTitleBox">
+          <span class="sessionsTitle">Sessions</span>
+          <span class="sessionsTitleBackground">Sessions</span>
+        </div>
+
+        <div class="sessions">
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">First session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/222@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>In the first session, we will consider design thinking and its understanding as well as different models of design thinking and the design process, and we will better remember this process with various examples.<br /> We will also discuss problem solving techniques together and understand the concept of user experience and usability.</span>
+          </div>
+          
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Second session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/2676@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>In the second session, we will discuss service design, first discussing “What is a service?”<br /> Then we practice how to create the right conditions for them in the long run to stay loyal to our product based on the actual needs of our audience and to create solutions that will reduce the hassle of the user in the short run.<br /> We are also going to create a way to measure the performance of our product so that we can ensure the quality of the design solution’s performance.</span>
+          </div>
+          
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Third session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/222@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>In the third session we will discuss the most important design phase, which is the initial research and review process.<br /> We will explore user experience research and user research methods.<br /> At the end of the session we will be able to build user personas as well as user scenarios and narrated user stories in the product.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Fourth session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/72676@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>Building experience is one of the key points in product management and design. But how does this experience come about?<br /> The experience is made by creating a unique feeling in the mind. In the fourth session, we will learn how to identify users’ behavior and motivate them as well as build stimuli to drive the minds of the target audience together.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Fifth session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/222@2x.png" alt="profileImg" class="profileImg" />
+              <img src="/images/4444444@2x.png" alt="profileImg2" />
+            </div>
+            <span>It’s time to get started.The fifth session is intended to capture the audiences’ minds and create a new experience for them, like a humorist, sculptor or painter.But with one big difference - and the difference is that we will solve their problems at the same time by targeting users with product design. The fifth session is actually the beginning of the user experience design process, where we will learn about the concepts of user-centered design and data-driven design.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Sixth session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/22222@2x.png" alt="profileImg" class="profileImg" />
+              <img src="/images/111@2x.png" alt="profileImg2" />
+            </div>
+            <span>Today, the world is a world of interaction, in the sixth session we understand users trying to find the best way to interact with them and bring the emotions, perceptions, and awareness of the users to the forefront.<br /> We will explore the concept of Interaction Design and also explore it in terms of user experience design, usability and intuitive interface design. In the second part of the sixth session, we go into marketing and practice the marketing strategy of digital products together, as well as discussing the appropriate digital marketing toolkit.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Seventh session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/photo_2018-11-05_23-52-41@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>In the seventh session, we are in the world of product management. We will examine software companies’ structures as well as in-team and in-team interactions. In this session, we will look at the duties and characteristics of product team members as well as identify product strategies and learn about different software development methodologies. And finally, we’ll look at a product designer’s red line.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Eight session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/hasan1@2x.png" alt="profileImg" class="profileImg" />
+              <img src="/images/7272@2x.png" alt="profileImg2" />
+            </div>
+            <span>In this session, we will get our hands into the color, learn the basics of user interface design with colored hands, and learn about various interface design tools. We also explore the concepts and fundamentals of iconography and learn about testing the usability of icon design and finally learn the process of visualization in digital products from idea to implementation.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Ninth session</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/hasan1@2x.png" alt="profileImg" class="profileImg" />
+              <img src="/images/23423@2x.png" alt="profileImg2" />
+            </div>
+            <span>In the ninth session we will continue to immerse ourselves in the world of color and image, identifying the elements of the interface and learning how to properly represent the data.<br /> We also remotely team up with Moslem Ebrahimi, a typeface designer, and look at the typography of digital products from the perspective of someone who has had a lifetime of concern for typefaces and Persian fonts.</span>
+          </div>
+
+          <div class="sessionHeader">
+            <span class="sessionNumberTitle">Tenth meeting</span>
+            <div class="sessionsClock">
+              <img src="/images/Livello_5@2x.png" alt="clockImg" class="clockImg" />
+              <span>Duration: 6 hour</span>
+            </div>
+            <div class="sessionsCalendar">
+              <img src="/images/event@2x.png" alt="calendarImg" class="calendarImg" />
+              <span>Date: November 5</span>
+            </div>
+            <div class="sessionsView">
+              <img src="/images/Grupo 845@2x.png" alt="viewImg" class="viewImg" />
+              <span>View video</span>
+            </div>
+          </div>
+          <div class="sessionBody">
+            <div class="profilePicturesBox">
+              <img src="/images/hasan1@2x.png" alt="profileImg" class="profileImg" />
+            </div>
+            <span>In the 10th session we will start with the software and together we will learn what design software capabilities are and what ideas can be made with them and that they are not as complex as the cockpit!</span>
+          </div>
+
+          </div>
+
+        </div>
+      </div>
+  );
+}
+
+export default App;
